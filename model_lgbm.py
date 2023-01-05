50ca0b6b8ed3454cf0428b9d3f32956eea19b77e
handsomevictor <857730556@qq.com>
2023-01-05 13:28:56 +0100

    updates

--- model_lgbm.py
+++ model_lgbm.py
@@ -1,4 +1,20 @@
-import lightgbm as lgb
+import xgboost as xgb
+
+params = {
+    'objective': 'binary:logistic',
+    'eval_metric': 'auc',
+    'eta': 0.1,
+    'max_depth': 6,
+    'subsample': 0.9,
+    'colsample_bytree': 0.9,
+    'min_child_weight': 1,
+    'alpha': 0,
+    'lambda': 1,
+    'seed': 42,
+    'nthread': 4,
+    'silent': True
+}
+
 
 if __name__ == '__main__':
     pass
