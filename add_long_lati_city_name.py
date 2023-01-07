a084ea8269dc71c381d86a34ff7d4813830a7c6d
handsomevictor <857730556@qq.com>
2023-01-07 16:25:45 +0100

    updates

--- add_long_lati_city_name.py
+++ add_long_lati_city_name.py
@@ -27,30 +27,47 @@ def find_city_name(lati, long):
         county = location.raw['address']['county']
     except:
         county = None
-    return neighborhood, suburb, county
+    try:
+        city = location.raw['address']['city']
+    except:
+        city = None
+    return neighborhood, suburb, county, city
 
 
-def get_geo_info_concurrent(max_workers=10):
-    # 不允许这样，超过最大request limit
-    train_df = pd.read_csv('train.csv', index_col=0)
-    with ProcessPoolExecutor(max_workers=50) as pool:
-        res = list(tqdm(pool.map(find_city_name, train_df['Latitude'], train_df['Longitude']), total=len(train_df)))
-    return res
+# def get_geo_info_concurrent(max_workers=10):
+#     # 不允许这样，超过最大request limit
+#     train_df = pd.read_csv('train.csv', index_col=0)
+#     with ProcessPoolExecutor(max_workers=50) as pool:
+#         res = list(tqdm(pool.map(find_city_name, train_df['Latitude'], train_df['Longitude']), total=len(train_df)))
+#     return res
 
 
 if __name__ == '__main__':
     train_df = Data('train', scaler_type='none', use_original_data=True).data
-
+    train_df = train_df.reset_index()
     # add neighborhood and county name
-    for i in tqdm(range(10)):  # len(train_df))
+    for i in tqdm(range(len(train_df))):
         lati = train_df.iloc[i]['Latitude']
         long = train_df.iloc[i]['Longitude']
-        neighborhood, suburb, county = find_city_name(lati, long)
+        neighborhood, suburb, county, city = find_city_name(lati, long)
         train_df.loc[i, 'Neighborhood'] = neighborhood
         train_df.loc[i, 'County'] = county
         train_df.loc[i, 'Suburb'] = suburb
+        train_df.loc[i, 'City'] = city
 
-    # train_df['Neighborhood'], train_df['County'], train_df['Suburb'] = zip(
-    #     *train_df.apply(lambda x: find_city_ name(x['Latitude'], x['Longitude']), axis=1))
     train_df.to_csv('train_add_geo_info.csv')
 
+    prediction_data = Data('test', scaler_type='none').processed_data
+    prediction_data = prediction_data.reset_index()
+
+    for i in tqdm(range(len(prediction_data))):
+        lati = prediction_data.iloc[i]['Latitude']
+        long = prediction_data.iloc[i]['Longitude']
+        neighborhood, suburb, county, city = find_city_name(lati, long)
+
+        prediction_data.loc[i, 'Neighborhood'] = neighborhood
+        prediction_data.loc[i, 'County'] = county
+        prediction_data.loc[i, 'Suburb'] = suburb
+        prediction_data.loc[i, 'City'] = city
+
+    prediction_data.to_csv('test_add_geo_info.csv')
